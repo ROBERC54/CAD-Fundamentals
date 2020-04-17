726dbb9ce228106f558af3cb6481ce7a1b8d8156
Christian Roberts <christian.m.roberts@outlook.com>
2020-04-17 16:13:39 -0400

    Gave birth to Groundwork.  Began new delivery method viaConsoleApp

---
+++ viaConsoleApp/viaConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace viaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

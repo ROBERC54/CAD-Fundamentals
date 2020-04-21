19c18909a98e99046caff756fd5e4cbb21cbfb5b
Christian Roberts <christian.m.roberts@outlook.com>
2020-04-21 08:37:07 -0400

    Removed an anomylous button

--- viaConsoleApp/viaConsoleApp/Program.cs
+++ viaConsoleApp/viaConsoleApp/Program.cs
@@ -6,7 +6,31 @@ namespace viaConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+
+
+
+            /*for (count = -10; count < 5; count++)
+            {
+                //Console.WriteLine(count);
+                Console.WriteLine("Please type something.  I'll print it back to you");
+                string userinput = Console.ReadLine();
+                Console.WriteLine($"You wrote {userinput}!");
+            }*/
+
+            Console.WriteLine(count);
+            Console.WriteLine("Please type something.  I'll print it back to you");
+            string userinput = Console.ReadLine();
+            Console.WriteLine($"You wrote {userinput}!");
+
+            Console.WriteLine(count);
+            Console.WriteLine("Please type something.  I'll print it back to you");
+            string userinput = Console.ReadLine();
+            Console.WriteLine($"");
+            
+
+
+            //for( ; ; )
         }
     }
 }

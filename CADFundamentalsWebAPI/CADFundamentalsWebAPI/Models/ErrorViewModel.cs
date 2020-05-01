c73dfd2f1f33642d77afb4cd990dd414d3ec90d9
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-01 09:44:46 -0400

    Planning elements of EndProduct begun

---
+++ CADFundamentalsWebAPI/CADFundamentalsWebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CADFundamentalsWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

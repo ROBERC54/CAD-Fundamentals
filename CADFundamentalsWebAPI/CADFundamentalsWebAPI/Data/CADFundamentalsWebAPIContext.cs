a3cb106d3ba78da464af13126e9cd765e980e965
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-01 11:37:49 -0400

    Created a database context class, including a DbSet representative of the UberCategory table

---
+++ CADFundamentalsWebAPI/CADFundamentalsWebAPI/Data/CADFundamentalsWebAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CADFundamentalsWebAPI.Models;
+
+namespace CADFundamentalsWebAPI.Data
+{
+    public class CADFundamentalsWebAPIContext : DbContext
+    {
+        public CADFundamentalsWebAPIContext(DbContextOptions<CADFundamentalsWebAPIContext> options)
+            : base(options)
+        { 
+        }
+        public DbSet<UberCategory> UberCategory { get; set; }
+    }
+}

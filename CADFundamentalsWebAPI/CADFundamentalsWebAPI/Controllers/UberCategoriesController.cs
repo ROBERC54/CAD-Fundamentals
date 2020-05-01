2c923b1fb5a64d1c2fa775ff603f9f9375ef5b09
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-01 13:59:19 -0400

    Ported the majority of UberCategories index content to a FullList page + updated the appropriate links within Create, Edit, Details, and Delete to redirect there.

--- CADFundamentalsWebAPI/CADFundamentalsWebAPI/Controllers/UberCategoriesController.cs
+++ CADFundamentalsWebAPI/CADFundamentalsWebAPI/Controllers/UberCategoriesController.cs
@@ -24,6 +24,10 @@ namespace CADFundamentalsWebAPI.Controllers
         {
             return View(await _context.UberCategory.ToListAsync());
         }
+        public async Task<IActionResult> FullList()
+        {
+            return View(await _context.UberCategory.ToListAsync());
+        }
 
         // GET: UberCategories/Details/5
         public async Task<IActionResult> Details(int? id)

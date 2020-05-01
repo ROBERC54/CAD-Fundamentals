7ade8e14ee9d5a5d2daa165b5c9083656f05352f
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-01 11:25:48 -0400

    Add NuGet packages for Microsoft.EntityFrameworkCore.SqlServer.  Also created a stock model class for the UberCategories

---
+++ CADFundamentalsWebAPI/CADFundamentalsWebAPI/Models/UberCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CADFundamentalsWebAPI.Models
+{
+    public class UberCategory
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LatestReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Complexity { get; set; }
+        public decimal Price { get; set; }
+    }
+}

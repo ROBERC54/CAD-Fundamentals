0d15d1849e03040f74d38734b86b80eb58447da3
Christian Roberts <christian.m.roberts@outlook.com>
2020-05-15 14:11:17 -0400

    Ported the typical index view for Topics to a FullList view.  The index view now mirrors the higherlevel (UberCategory) index view with associated TopicNavCollapse in terms of structure

--- CADFundamentalsWebAPI/CADFundamentalsWebAPI/Controllers/TopicsController.cs
+++ CADFundamentalsWebAPI/CADFundamentalsWebAPI/Controllers/TopicsController.cs
@@ -24,6 +24,10 @@ namespace CADFundamentalsWebAPI.Controllers
         {
             return View(await _context.Topic.ToListAsync());
         }
+        public async Task<IActionResult> FullList()
+        {
+            return View(await _context.Topic.ToListAsync());
+        }
 
         // GET: Topics/Details/5
         public async Task<IActionResult> Details(int? id)
